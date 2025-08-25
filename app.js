b22f75070a24aa460314cd111bef46a6543f1d68
Erno <erno.kahkonen@gmail.com>
2025-08-25 15:43:54 +0300

    health check

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.get('/version', (req, res) => {
   res.send('4') // change this string to ensure a new version deployed
 })
 
-
-
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
