711d0ca518dfd7968d9a8b463dd2488c70049eb7
Erno <erno.kahkonen@gmail.com>
2025-08-25 20:20:39 +0300

    unfail

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-consoleff
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
